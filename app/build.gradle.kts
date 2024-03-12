a56081f71ed712f0a146f5c5cd8b5ba337784aae
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-03-12 13:56:58 +0700

    feat: add retrofit+gson dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     val fragmentVersion = "1.6.2"
     // Java language implementation
