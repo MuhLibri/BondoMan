851001d76b76d149645b94e1c93540aaa180d52c
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-03-12 07:53:16 +0700

    feat: placeholder for main activity

--- app/src/main/java/com/sleepee/bondoman/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/sleepee/bondoman/presentation/activity/MainActivity.kt
@@ -2,11 +2,29 @@ package com.sleepee.bondoman.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.OnBackPressedCallback
 import com.sleepee.bondoman.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val logoutButton = findViewById<Button>(R.id.logoutButton)
+        logoutButton.setOnClickListener {
+            logout()
+        }
+
+        // Handle back button => Minimize the app, no logout
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                moveTaskToBack(true)
+            }
+        })
+    }
+
+    private fun logout() {
+        finish()
     }
 }
