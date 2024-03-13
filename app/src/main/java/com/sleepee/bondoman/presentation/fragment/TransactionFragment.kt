85bee9a1dacfee0d74c25c7c2c32b56b3cec9ae1
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2024-03-13 17:24:18 +0700

    feat: add transaction button

--- app/src/main/java/com/sleepee/bondoman/presentation/fragment/TransactionFragment.kt
+++ app/src/main/java/com/sleepee/bondoman/presentation/fragment/TransactionFragment.kt
@@ -4,17 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.sleepee.bondoman.R
+import com.sleepee.bondoman.databinding.DialogAddTransactionBinding
+import com.sleepee.bondoman.databinding.FragmentTransactionBinding
 
 class TransactionFragment: Fragment() {
+
+    private lateinit var binding: FragmentTransactionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_transaction, container, false)
+        binding = FragmentTransactionBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fab.setOnClickListener {
+            showAddTransactionDialog()
+        }
+    }
+
+    // sample function
+
+    private fun showAddTransactionDialog() {
+        val dialogBinding = DialogAddTransactionBinding.inflate(layoutInflater)
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("add new transaction")
+            .setView(dialogBinding.root)
+            .setPositiveButton("Save") { _, _ ->
+                Toast.makeText(
+                    requireContext(),
+                    "Your transaction is: ${dialogBinding.editText.text}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
     }
 
 }
