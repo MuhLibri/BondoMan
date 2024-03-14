0fc0c917df7acd9cfef98aa1dc0fcf07c4dce118
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2024-03-14 14:29:55 +0700

    feat: add transaction validation

--- app/src/main/java/com/sleepee/bondoman/presentation/activity/AddTransactionActivity.kt
+++ app/src/main/java/com/sleepee/bondoman/presentation/activity/AddTransactionActivity.kt
@@ -2,12 +2,16 @@ package com.sleepee.bondoman.presentation.activity
 
 import android.R
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.sleepee.bondoman.databinding.ActivityAddTransactionBinding
+
 
 class AddTransactionActivity: AppCompatActivity() {
 
@@ -26,11 +30,38 @@ class AddTransactionActivity: AppCompatActivity() {
         title = "Add New Transaction"
         setupCategoryDropdown()
         binding.addTransactionButton.setOnClickListener {
-            val title = binding.title.text.toString()
-            val amount = binding.amount.text.toString()
-            val location = binding.location.text.toString()
+            val title = binding.title
+            val amount = binding.amount
+            val location = binding.location
+
+            validateAddActivity(title, amount, location)
         }
 
+    }
+
+    private fun validateAddActivity(title: EditText, amount: EditText, location: EditText) {
+        if (title.length() == 0) {
+            title.error = "Title cannot be blank"
+            return
+        }
+        if (amount.length() == 0) {
+            amount.error = "Amount cannot be blank"
+            return
+        }
+        if (location.length() == 0) {
+            location.error = "Location cannot be blank"
+            return
+        }
+        if (!TextUtils.isDigitsOnly(amount.text)) {
+            amount.error = "Amount must be an integer"
+            return
+        }
+        if (amount.text.toString()[0] == '0') {
+            amount.error = "Amount must not be 0"
+            return
+        }
+
+        Toast.makeText(this, "Okay", Toast.LENGTH_SHORT).show()
     }
 
     private fun setupCategoryDropdown() {
